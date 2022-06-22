57e85684fec8399cc33a7b867501da0696f7ebf1
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-22 15:41:08 +0530

    Feat: Add Colours Hex Codes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Sora", "sans-serif"],
-      }
+      },
+      colors: {
+        "japanese-indigo": "#243B55",
+        "dark-gunmetal": "#141E30",
+        "dark-bg": "#0E131F",
+        primary: "#0066FF",
+        "oxford-blue": "#002b46",
+        "blue-pigment": "#363795",
+      },
     },
   },
   plugins: [],
