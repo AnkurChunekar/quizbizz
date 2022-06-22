c1c0366e256774b72d93f9a1acf3637d96df6531
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-22 15:43:53 +0530

    Feat: Reexport Componenet from index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Navbar } from "./Navbar";
+export { Hero } from "./Hero";
+export { Card } from "./Card";
+export { QuizSection } from "./QuizSection";
+export { CategoriesSection } from "./CategoriesSection";
+export { Footer } from "./Footer";
+export { RuleBox } from "./RuleBox";
