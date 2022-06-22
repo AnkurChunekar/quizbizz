82b7218aadb467b9f62096a1e66f3faf66a1a3db
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-22 15:41:54 +0530

    Feat: Add Navbar Component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex flex-wrap justify-between items-center p-4 mx-auto text-neutral-50 bg-gray-800 rounded-b-xl w-full">
+      <h2 className="font-bold text-xl">GoalQuiz</h2>
+      <p>Logout</p>
+    </nav>
+  );
+};
