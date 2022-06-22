e535553820c0ec12697d1a5d7bb39264ea443615
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-06-22 15:49:37 +0530

    Feat: Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Navbar, Footer } from "./components";
+import { Home, Quiz, Result, Rules } from "./pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-dark-bg">
+      <div className="text-neutral-50 min-h-screen max-w-screen-xl mx-auto flex flex-col gap-6">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
